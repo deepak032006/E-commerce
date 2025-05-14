e2efd8756f23d877844fb9ed22c65b98c04352f3
Deeoak sharma <demondark7678@gmail.com>
2025-05-14 08:01:53 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: parseInt(process.env.PORT) || 5173,
-    host: '0.0.0.0',
-    allowedHosts: ['e-commerce-6-9tm1.onrender.com'],
-  }
+  plugins: [react(), tailwindcss(),],
+    
 })
