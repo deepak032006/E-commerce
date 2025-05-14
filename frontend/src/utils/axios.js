f6a1525f847390cb385e42065d00547110cd2ef3
Deeoak sharma <demondark7678@gmail.com>
2025-05-14 07:21:39 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://e-commerce-5-48ed.onrender.com',
   withCredentials:true
 });
 
