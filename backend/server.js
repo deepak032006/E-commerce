60cc02fec5a462e65d118a50ad9c809c804fa48c
deeoak sharma <demondark7678@gmail.com>
2025-05-26 16:52:09 +0530

    Update: your message here

--- backend/server.js
+++ backend/server.js
@@ -38,14 +38,14 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-if (process.env.NODE_ENV === 'production') {
- const dirPath = path.resolve()
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === 'production') {
+//  const dirPath = path.resolve()
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", 'dist', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", 'dist', 'index.html'));
+//   });
+// }
 
 // Start server
 const PORT = process.env.PORT || 5000;
